66cfd4d381619970bc45834a88a37684e56b9f36
nina <ninajl@gmail.com>
2015-03-15 14:23:59 -0600

    Create Residents table

---
+++ db/migrate/20150315201920_create_residents.rb
@@ -0,0 +1,12 @@
+class CreateResidents < ActiveRecord::Migration
+  def change
+    create_table :residents do |t|
+      t.string :first_name
+      t.string :last_name
+      t.integer :unit
+      t.boolean :tower
+      t.string :telephone
+      t.string :email
+    end
+  end
+end
