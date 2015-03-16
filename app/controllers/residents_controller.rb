a2298074caa36078c904bed5ce2abaf104501042
nina <ninajl@gmail.com>
2015-03-15 23:21:24 -0600

    Create edit and delete buttons to resident index. Add notice to completed resident controller.

--- app/controllers/residents_controller.rb
+++ app/controllers/residents_controller.rb
@@ -18,7 +18,25 @@ class ResidentsController < ApplicationController
     end
   end
 
-  
+  def edit
+    @resident = Resident.find(params[:id])
+  end
+
+  def update
+    @resident = Resident.find(params[:id])
+    if @resident.update(resident_params)
+      redirect_to residents_path, notice: "Resident was successfully updated!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @resident = Resident.find(params[:id])
+    @resident.destroy
+    redirect_to residents_path, notice: "Resident was successfully deleted!"
+  end
+
 
   private
   def resident_params
